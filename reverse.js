b35894e9f84bbafc3626579c2e90dc735b86903e
Abubakar Adeni <78494964+abubakar-adeni@users.noreply.github.com>
2023-08-01 19:25:35 +0700

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,8 +1,13 @@
 const reverseWords = (kalimat) => {
   if (typeof kalimat === "string") {
     if (kalimat.toLowerCase() === kalimat) {
-      const hasil = kalimat.split(" ").reverse().join(" ");
-      console.log(hasil);
+      const words = kalimat.split(" ");
+      if (words.length < 2) {
+        console.log("Inputan Harus Lebih Dari 1 Kata");
+      } else {
+        const hasil = words.reverse().join(" ");
+        console.log(hasil);
+      }
     } else {
       console.log("Parameter (kalimat) harus ditulis dengan huruf kecil.");
     }
